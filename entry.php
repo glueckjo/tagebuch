4f884b638be215337402a3248cbd98e8495e793c
glueckjo <45166738+glueckjo@users.noreply.github.com>
2018-12-03 15:50:58 +0100

    Update entry.php

--- entry.php
+++ entry.php
@@ -1,49 +1,49 @@
 <?php
-class entry{
+	class entry{
 
-	protected $entryID = "";
-	protected $content = "";
-	protected $userName = "";
-	protected $visible =  "";
-	protected $entryPublic = "";
-	
-	function __construct($content, $userName, $visible, $entryPublic){
-		$this->content = $content;
-		$this->userName = $userName;
-		$this->visible = $visible;
-		$this->entryPublic = $entryPublic;
-	}
+		protected $entryID = "";
+		protected $content = "";
+		protected $userName = "";
+		protected $visible =  "";
+		protected $entryPublic = "";
 
-	function setEntryID($entryID){
-		$this->entryID = $entryID;
+		function __construct($content, $userName, $visible, $entryPublic){
+			$this->content = $content;
+			$this->userName = $userName;
+			$this->visible = $visible;
+			$this->entryPublic = $entryPublic;
+		}
+
+		function setEntryID($entryID){
+			$this->entryID = $entryID;
+		}
+
+		function setViseble($visible){
+			$this->visible = $visible;
+		}
+
+		function setEntryPublic($entryPublic){
+			$this->entryPublic = $entryPublic;
+		}
+
+		function getContent(){
+			return $this->content;
+		}
+
+		function getEntryID(){
+			return $this->entryID;
+		}
+
+		function getUserName(){
+			return $this->userName;
+		}
+
+		function getEntryPublic(){
+			return $this->entryPublic;
+		}
+
+		function getVisible(){
+			return $this->visible;
+		}
 	}
-	
-	function setViseble($visible){
-		$this->visible = $visible;
-	}
-	
-	function setEntryPublic($entryPublic){
-		$this->entryPublic = $entryPublic;
-	}
-	
-	function getContent(){
-		return $this->content;
-	}
-	
-	function getEntryID(){
-		return $this->entryID;
-	}
-	
-	function getUserName(){
-		return $this->userName;
-	}
-	
-	function getEntryPublic(){
-		return $this->entryPublic;
-	}
-	
-	function getVisible(){
-		return $this->visible;
-	}
-}
 ?>
