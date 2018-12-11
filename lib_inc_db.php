a512bdf3bdcc8af68924b6e223298814da34aa9a
glueckjo <jonas.glueck@gmx.net>
2018-12-11 09:27:46 +0100

    Passwort für MariaDB wird in einer nicht getrackten Datei gespeichert. 'db_pwd.txt' muss dass Passwort in der ersten Zeile enthalten, Leestring sollte auch funktionieren.

--- lib_inc_db.php
+++ lib_inc_db.php
@@ -1,8 +1,10 @@
 <?php 
 
 	function connectDB(string $dbName){
+		$lines = file('db_pwd.txt');
+		$pwd = $lines[0];
 		try {
-			$db = new PDO('mysql:host=localhost;dbname='.$dbName, 'root', '');
+			$db = new PDO('mysql:host=localhost;dbname='.$dbName, 'root', $pwd);
 			return $db;
 		} catch (PDOException $e) {
 			//die('DB_ERROR: '.$e->getMessage());
